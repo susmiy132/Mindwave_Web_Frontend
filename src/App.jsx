72eaf74b0400127050ea16897d80d0c77e48b4ab
USER <thakursusmi94@gmail.com>
2025-06-13 19:51:22 +0545

    App.jsx file import

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-// import React from "react";
+import React from "react";
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Navbar from "./components/Navbar";
 // import RegisterForm from "./pages/register";
 // import LoginForm from "./pages/login";
 // import { ToastContainer } from "react-toastify"; // ✅ enable toast
-// import "react-toastify/dist/ReactToastify.css";
+import "react-toastify/dist/ReactToastify.css";
 
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
+function App() {
+  return (
+    // <Router>
+    //   <Navbar />
 
-//       {/* Toast container must be outside Routes to work globally */}
-//       <ToastContainer position="top-right" autoClose={3000} />
+    //   {/* Toast container must be outside Routes to work globally */}
+    //   <ToastContainer position="top-right" autoClose={3000} />
 
-//       <div className="pt-20 px-4 w-full">
-//         <Routes>
-//           <Route path="/register" element={<RegisterForm />} />
-//           <Route path="/login" element={<LoginForm />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
+    //   <div className="pt-20 px-4 w-full">
+    //     <Routes>
+    //       <Route path="/register" element={<RegisterForm />} />
+    //       <Route path="/login" element={<LoginForm />} />
+    //     </Routes>
+    //   </div>
+    // </Router>
+  );
+}
 
-// export default App;
+export default App;
