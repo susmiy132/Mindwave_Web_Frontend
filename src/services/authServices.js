022abbd00daea7fced4ca8cee5884984c39646fb
USER <thakursusmi94@gmail.com>
2025-08-01 18:46:10 +0545

    added authservices

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,35 +1,35 @@
-// // import {registerUserApi} from "../api/authApi";
+// import {registerUserApi} from "../api/authApi";
 
-// // export const registerUserService = async (fromData) => {
-// //     try{
-// //         const response = await registerUserApi(fromData)
-// //         return response.data    //body of response
-// //     }catch(err){
-// //         throw err.response?.data || {message: "Registration failed"}
-// //     }
-// // };
+// export const registerUserService = async (fromData) => {
+//     try{
+//         const response = await registerUserApi(fromData)
+//         return response.data    //body of response
+//     }catch(err){
+//         throw err.response?.data || {message: "Registration failed"}
+//     }
+// };
 
-// // export const fetchUserProfile = async (token) => {
-// //   try {
-// //     const response = await axios.get("http://localhost:5050/api/auth/me", {
-// //       headers: { Authorization: `Bearer ${token}` },
-// //     });
-// //     return response.data.user; // return just the user object
-// //   } catch (error) {
-// //     throw error.response?.data || { message: "Failed to fetch user profile" };
-// //   }
-// // };
-
-// import { registerUserApi, loginUserApi, fetchUserApi } from "../api/authApi";
-
-// export const registerUserService = async (formData) => {
+// export const fetchUserProfile = async (token) => {
 //   try {
-//     const response = await registerUserApi(formData);
-//     return response.data;
-//   } catch (err) {
-//     throw err.response?.data || { message: "Registration failed" };
+//     const response = await axios.get("http://localhost:5050/api/auth/me", {
+//       headers: { Authorization: `Bearer ${token}` },
+//     });
+//     return response.data.user; // return just the user object
+//   } catch (error) {
+//     throw error.response?.data || { message: "Failed to fetch user profile" };
 //   }
 // };
+
+import { registerUserApi, loginUserApi, fetchUserApi } from "../api/authApi";
+
+export const registerUserService = async (formData) => {
+  try {
+    const response = await registerUserApi(formData);
+    return response.data;
+  } catch (err) {
+    throw err.response?.data || { message: "Registration failed" };
+  }
+};
 
 // export const loginUserService = async (formData) => {
 //   try {
