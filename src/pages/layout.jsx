e72bc068f440c00e1d3caf1c325400ea27c0cb3a
USER <thakursusmi94@gmail.com>
2025-08-01 18:26:46 +0545

    added layout

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,17 +1,17 @@
-// // src/pages/Layout.jsx
-// import React from "react";
-// import Sidebar from "../components/Sidebar";
-// import { Outlet } from "react-router-dom";
+// src/pages/Layout.jsx
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import { Outlet } from "react-router-dom";
 
-// const Layout = () => {
-//   return (
-//     <div className="flex">
-//       <Sidebar />
-//       <main className="ml-64 pt-20 p-8 bg-gray-100 min-h-screen w-full">
-//         <Outlet />
-//       </main>
-//     </div>
-//   );
-// };
+const Layout = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <main className="ml-64 pt-20 p-8 bg-gray-100 min-h-screen w-full">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
 
-// export default Layout;
+export default Layout;
