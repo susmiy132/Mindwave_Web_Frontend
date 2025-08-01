89cdc7ff8413f0bb491eb90c33f36ec83505d897
USER <thakursusmi94@gmail.com>
2025-08-01 19:03:41 +0545

    added progress services

--- src/services/progressServices.js
+++ src/services/progressServices.js
@@ -1,25 +1,25 @@
-// // src/services/progressService.js
-// import axios from "axios";
+// src/services/progressService.js
+import axios from "axios";
 
-// const API_BASE = "http://localhost:5050/api"; // replace with your actual backend base URL
+const API_BASE = "http://localhost:5050/api"; // replace with your actual backend base URL
 
-// export const fetchMeditations = async (token) => {
-//   const res = await axios.get(`${API_BASE}/meditations`, {
-//     headers: { Authorization: `Bearer ${token}` },
-//   });
-//   return res.data;
-// };
+export const fetchMeditations = async (token) => {
+  const res = await axios.get(`${API_BASE}/meditations`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
 
-// export const fetchJournals = async (token) => {
-//   const res = await axios.get(`${API_BASE}/journals`, {
-//     headers: { Authorization: `Bearer ${token}` },
-//   });
-//   return res.data;
-// };
+export const fetchJournals = async (token) => {
+  const res = await axios.get(`${API_BASE}/journals`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
 
-// export const fetchMoodData = async (token) => {
-//   const res = await axios.get(`${API_BASE}/mood`, {
-//     headers: { Authorization: `Bearer ${token}` },
-//   });
-//   return res.data;
-// };
+export const fetchMoodData = async (token) => {
+  const res = await axios.get(`${API_BASE}/mood`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
