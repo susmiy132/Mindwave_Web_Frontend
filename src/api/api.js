3dabdc6a80df7b67d0ec3a83c267c29560bf9bf1
USER <thakursusmi94@gmail.com>
2025-08-01 18:41:05 +0545

    connected to the backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,21 +1,21 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const API_URL = import.meta.env.VITE_API_BASE_URL ||
-//     "localhost:5050/api" // fallback 
-// const instance = axios.create(
-//     {
-//         baseURL: API_URL,
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     }
-// )
+const API_URL = import.meta.env.VITE_API_BASE_URL ||
+    "localhost:5050/api" // fallback 
+const instance = axios.create(
+    {
+        baseURL: API_URL,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+)
 
-// instance.interceptors.request.use((config) => {
-//     const token = localStorage.getItem("token") 
-//     if(token){
-//         config.headers.Authorixation = "Bearer" + token
-//     }
-// })
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token") 
+    if(token){
+        config.headers.Authorixation = "Bearer" + token
+    }
+})
 
-// export default instance;
+export default instance;
