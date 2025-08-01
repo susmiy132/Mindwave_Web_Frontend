23601dc1ec7e02d37a9621ff3cc8229c8c4cd2d1
USER <thakursusmi94@gmail.com>
2025-08-01 18:34:33 +0545

    added useRegisterUser

--- src/hooks/useRegisterUser.js
+++ src/hooks/useRegisterUser.js
@@ -1,32 +1,32 @@
-// import { useState } from "react";
-// import axios from "axios";
-// import { registerUserService } from "../services/authServices";
+import { useState } from "react";
+import axios from "axios";
+import { registerUserService } from "../services/authServices";
 
-// export const useRegisterUser = () => {
-//     const [isLoading, setIsLoading] = useState(false)
-//     const [error, setError] = useState(null)
-//     const [data, setData] = useState(null)
+export const useRegisterUser = () => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const [data, setData] = useState(null)
 
-//     const register = async (formData) => {
-//         // when user click an event/button
-//         setIsLoading(true)
-//         setError(null) // clear state
-//         setData(null) // clear state
-//         try{
-//             const response = await registerUserService(formData)
-//             setData(response)
-//             return response
-//         }catch(err){
-//             setError(err)
-//             return null
-//         }finally{
-//             setIsLoading(false)
-//         }
-//     }
-//     return {
-//         register,
-//         isLoading,
-//         data,
-//         error
-//     }
-// }
+    const register = async (formData) => {
+        // when user click an event/button
+        setIsLoading(true)
+        setError(null) // clear state
+        setData(null) // clear state
+        try{
+            const response = await registerUserService(formData)
+            setData(response)
+            return response
+        }catch(err){
+            setError(err)
+            return null
+        }finally{
+            setIsLoading(false)
+        }
+    }
+    return {
+        register,
+        isLoading,
+        data,
+        error
+    }
+}
