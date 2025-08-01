f4e4fe6e9bf35af4cc8160470492f464b5e9307f
USER <thakursusmi94@gmail.com>
2025-08-01 18:38:28 +0545

    added journal hooks

--- src/hooks/usejournals.js
+++ src/hooks/usejournals.js
@@ -1,30 +1,30 @@
-// import { useEffect, useState } from "react";
-// import { fetchAllJournals } from "../services/journalServices";
+import { useEffect, useState } from "react";
+import { fetchAllJournals } from "../services/journalServices";
 
-// const useJournals = () => {
-//   const [journals, setJournals] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState("");
+const useJournals = () => {
+  const [journals, setJournals] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-//   const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-//   const loadJournals = async () => {
-//     try {
-//       setLoading(true);
-//       const data = await fetchAllJournals(token);
-//       setJournals(data);
-//     } catch (err) {
-//       setError(err.message);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
+  const loadJournals = async () => {
+    try {
+      setLoading(true);
+      const data = await fetchAllJournals(token);
+      setJournals(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-//   useEffect(() => {
-//     loadJournals();
-//   }, []);
+  useEffect(() => {
+    loadJournals();
+  }, []);
 
-//   return { journals, loading, error, reload: loadJournals };
-// };
+  return { journals, loading, error, reload: loadJournals };
+};
 
-// export default useJournals;
+export default useJournals;
