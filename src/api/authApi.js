822394f4a103dcfd4df8f70b982c727172a4f039
USER <thakursusmi94@gmail.com>
2025-08-01 18:42:03 +0545

    added authApi

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,11 +1,11 @@
-// // import axios from "./api"
+// import axios from "./api"
 
-// // export const registerUserApi = (data) => axios.post("/auth/register", data)
-// // export const loginUserApi = (data) => axios.post("/auth/login", data)
+// export const registerUserApi = (data) => axios.post("/auth/register", data)
+// export const loginUserApi = (data) => axios.post("/auth/login", data)
 
 
-// import axios from "./axios"; // uses the axios instance you created
+import axios from "./axios"; // uses the axios instance you created
 
-// export const registerUserApi = (data) => axios.post("/auth/register", data);
-// export const loginUserApi = (data) => axios.post("/auth/login", data);
-// export const fetchUserApi = () => axios.get("/auth/me"); // assuming you added this route
+export const registerUserApi = (data) => axios.post("/auth/register", data);
+export const loginUserApi = (data) => axios.post("/auth/login", data);
+export const fetchUserApi = () => axios.get("/auth/me"); // assuming you added this route
