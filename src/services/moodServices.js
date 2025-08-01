3344a989545438126fd6b074da27ab66ef3ca385
USER <thakursusmi94@gmail.com>
2025-08-01 19:02:45 +0545

    added mood services

--- src/services/moodServices.js
+++ src/services/moodServices.js
@@ -1,12 +1,12 @@
-// // src/services/moodServices.js
-// import api from "./api"; // Make sure this file exists
+// src/services/moodServices.js
+import api from "./api"; // Make sure this file exists
 
-// export const getMoods = async () => {
-//   const res = await api.get("/api/mood"); // Adjust path as per backend
-//   return res.data;
-// };
+export const getMoods = async () => {
+  const res = await api.get("/api/mood"); // Adjust path as per backend
+  return res.data;
+};
 
-// export const postMood = async (moodData) => {
-//   const res = await api.post("/api/mood", moodData);
-//   return res.data;
-// };
+export const postMood = async (moodData) => {
+  const res = await api.post("/api/mood", moodData);
+  return res.data;
+};
