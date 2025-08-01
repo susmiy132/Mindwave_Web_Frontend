b4ee773fcd9b61c230aa0046831316325f7834f4
USER <thakursusmi94@gmail.com>
2025-08-01 18:50:58 +0545

    added create Journal services

--- src/services/journalServices.js
+++ src/services/journalServices.js
@@ -1,41 +1,41 @@
 
 
-// const BASE_URL = "http://localhost:5050/api";
+const BASE_URL = "http://localhost:5050/api";
 
-// export const createJournal = async (form, token) => {
-//   const formData = new FormData();
-//   formData.append("title", form.title);
-//   formData.append("content", form.content);
-//   formData.append("mood", form.mood);
-//   if (form.image) formData.append("image", form.image);
+export const createJournal = async (form, token) => {
+  const formData = new FormData();
+  formData.append("title", form.title);
+  formData.append("content", form.content);
+  formData.append("mood", form.mood);
+  if (form.image) formData.append("image", form.image);
 
-//   const response = await fetch(`${BASE_URL}/journals`, {
-//     method: "POST",
+  const response = await fetch(`${BASE_URL}/journals`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: formData,
+  });
+
+  const data = await response.json();
+  if (!response.ok) throw new Error(data.message || "Failed to create journal");
+
+  return data;
+};
+
+// export const fetchAllJournals = async (token) => {
+//   const response = await fetch("http://localhost:5050/api/journals", {
+//     method: "GET",
 //     headers: {
 //       Authorization: `Bearer ${token}`,
 //     },
-//     body: formData,
 //   });
-
 //   const data = await response.json();
-//   if (!response.ok) throw new Error(data.message || "Failed to create journal");
-
+//   if (!response.ok) {
+//     throw new Error(data.message || "Failed to fetch journals");
+//   }
 //   return data;
 // };
-
-// // export const fetchAllJournals = async (token) => {
-// //   const response = await fetch("http://localhost:5050/api/journals", {
-// //     method: "GET",
-// //     headers: {
-// //       Authorization: `Bearer ${token}`,
-// //     },
-// //   });
-// //   const data = await response.json();
-// //   if (!response.ok) {
-// //     throw new Error(data.message || "Failed to fetch journals");
-// //   }
-// //   return data;
-// // };
 
 // export const fetchAllJournals = async (token) => {
 //   try {
@@ -59,7 +59,7 @@
 //   }
 // };
 
-// // Fetch journal by ID
+// Fetch journal by ID
 // export const getJournalById = async (id, token) => {
 //   const res = await fetch(`http://localhost:5050/api/journals/${id}`, {
 //     headers: {
