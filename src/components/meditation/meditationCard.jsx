6bfb62cc2872381e1dd26e9c8cb728b8446f2532
USER <thakursusmi94@gmail.com>
2025-08-01 18:13:25 +0545

    added meditationCard

--- src/components/meditation/meditationCard.jsx
+++ src/components/meditation/meditationCard.jsx
@@ -1,25 +1,25 @@
 
 
-// import React from "react";
+import React from "react";
 
-// const MeditationCard = ({ meditation, onDelete }) => {
-//   return (
-//     <div className="bg-white p-4 shadow-md rounded-lg mb-4 flex justify-between items-center">
-//       <div>
-//         <h2 className="text-lg font-semibold">{meditation.title}</h2>
-//         <p className="text-gray-600">{meditation.description}</p>
-//         <p className="text-sm text-gray-500">
-//           Duration: {meditation.duration || "N/A"} min
-//         </p>
-//       </div>
-//       <button
-//         className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
-//         onClick={() => onDelete(meditation._id)}
-//       >
-//         Delete
-//       </button>
-//     </div>
-//   );
-// };
+const MeditationCard = ({ meditation, onDelete }) => {
+  return (
+    <div className="bg-white p-4 shadow-md rounded-lg mb-4 flex justify-between items-center">
+      <div>
+        <h2 className="text-lg font-semibold">{meditation.title}</h2>
+        <p className="text-gray-600">{meditation.description}</p>
+        <p className="text-sm text-gray-500">
+          Duration: {meditation.duration || "N/A"} min
+        </p>
+      </div>
+      <button
+        className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
+        onClick={() => onDelete(meditation._id)}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
 
-// export default MeditationCard;
+export default MeditationCard;
